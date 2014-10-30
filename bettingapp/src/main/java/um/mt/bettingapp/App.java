57b530d3b72f22b14d68d93ea59629638c7f9a66
isaacchircop <isaacchircop@gmail.com>
2014-10-30 13:05:37 +0100

    first commit

---
+++ bettingapp/src/main/java/um/mt/bettingapp/App.java
@@ -0,0 +1,13 @@
+package um.mt.bettingapp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
