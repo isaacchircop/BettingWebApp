8123ac0f3a828772f9abb060f708846d69f3a0c0
isaacchircop <isaacchircop@gmail.com>
2014-11-25 15:58:09 +0100

    no message

--- ws/src/main/java/um/edu/mt/ws/UserManagerController.java
+++ ws/src/main/java/um/edu/mt/ws/UserManagerController.java
@@ -5,13 +5,20 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class UserManagerController {
 
 	@RequestMapping (value = "/registerUser", method = RequestMethod.POST)
-	public ResponseEntity<HttpStatus> addMessage(){
-		System.out.println("Hello World!");
+	public ResponseEntity<HttpStatus> regiserUser(@RequestParam(value = "username") final String username,
+												  @RequestParam(value = "password") final String password,
+												  @RequestParam(value = "name") final String name,
+												  @RequestParam(value = "surname") final String surname){
+		System.out.println(username);
+		System.out.println(password);
+		System.out.println(name);
+		System.out.println(surname);
 		return new ResponseEntity<HttpStatus>(HttpStatus.OK);
 	}
 	
